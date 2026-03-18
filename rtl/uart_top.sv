c987aadca38cf664fb3e8e60f91ca8f7a08826c5
Vageesha-S-R <176924830+Vageesha-S-R@users.noreply.github.com>
2026-03-18 17:37:17 +0100

    implementation of fifo_tx and fifo_rx

--- rtl/uart_top.sv
+++ rtl/uart_top.sv
@@ -6,13 +6,28 @@ module uart_top #(
     input rst_n,
     input logic[7:0] data_in,
     input logic data_in_valid,
-    output logic busy,
+    input logic read_en,
     output logic [7:0] data_out,
-    output logic data_out_valid
+    output logic data_out_valid,
+    output logic tx_fifo_full,
+    output logic rx_fifo_empty
 );
 
 logic baud_tick;
 logic serial_line;
+
+logic [7:0] tx_fifo_data_out;
+logic tx_fifo_empty;
+logic tx_fifo_read;
+logic tx_busy;
+
+logic [7:0] rx_data;
+logic rx_data_valid;
+logic rx_fifo_write;
+
+
+
+
 baud_gen #(
     .BAUD_RATE(BAUD_RATE),
     .CLK_FREQ(CLK_FREQ)) baud_gen_inst(
@@ -20,14 +35,25 @@ baud_gen #(
     .rst_n(rst_n),
     .baud_tick(baud_tick));
 
+fifo tx_fifo (
+    .clk(clk),
+    .rst_n(rst_n),
+    .write_en(data_in_valid),
+    .read_en(tx_fifo_read),
+    .data_in(data_in),
+    .data_out(tx_fifo_data_out),
+    .empty(tx_fifo_empty),
+    .full(tx_fifo_full)
+);
+
 uart_tx uart_tx_inst (
     .clk(clk),
     .rst_n(rst_n),
-    .data_in(data_in),
+    .data_in(tx_fifo_data_out),
     .baud_tick(baud_tick),
-    .data_valid(data_in_valid),
+    .data_valid(tx_fifo_read),
     .tx(serial_line),
-    .busy(busy)
+    .busy(tx_busy)
 );
 
 uart_rx uart_rx_inst (
@@ -35,9 +61,26 @@ uart_rx uart_rx_inst (
     .rst_n(rst_n),
     .rx(serial_line),
     .baud_tick(baud_tick),
-    .data_out(data_out),
-    .data_valid(data_out_valid)
+    .data_out(rx_data),
+    .data_valid(rx_data_valid)
 );
+
+fifo rx_fifo (
+    .clk(clk),
+    .rst_n(rst_n),
+    .write_en(rx_fifo_write),
+    .read_en(read_en),
+    .data_in(rx_data),
+    .data_out(data_out),
+    .empty(rx_fifo_empty),
+    .full()
+);
+
+
+assign tx_fifo_read  = !tx_fifo_empty && !tx_busy;
+assign rx_fifo_write = rx_data_valid; 
+
+assign data_out_valid = !rx_fifo_empty;
 
 endmodule
 
