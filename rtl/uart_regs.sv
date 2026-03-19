a49598fed904a0ac86555bdeaadb7976a614a57b
Vageesha-S-R <176924830+Vageesha-S-R@users.noreply.github.com>
2026-03-19 16:29:18 +0100

    implementation of uart_regs.sv

--- rtl/uart_regs.sv
+++ rtl/uart_regs.sv
@@ -5,7 +5,7 @@ module uart_regs (
     //CPU Interface
     input logic write_en,
     input logic read_en,
-    input logic [2:0] addr,
+    input logic [3:0] addr,
     input logic [7:0] wdata,
     output logic [7:0] rdata,
 
@@ -28,7 +28,79 @@ module uart_regs (
 
     // interrupt
     output logic irq
-    
+
 );
+localparam TX_ADDR      = 4'h0;
+localparam RX_ADDR      = 4'h4;
+localparam STATUS_ADDR  = 4'h8;
+localparam CONTROL_ADDR = 4'hC;
+
+logic [7:0] control_reg;
+logic [7:0] status_reg;
+
+always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        control_reg <= 0;
+        tx_fifo_write <= 0;
+        tx_fifo_wdata <= 0;
+    end
+    else begin
+        tx_fifo_write <= 0;
+        if (write_en) begin
+            case (addr)
+                TX_ADDR: begin
+                    if (!tx_fifo_full) begin
+                        tx_fifo_write <= 1;
+                        tx_fifo_wdata <= wdata;
+                    end
+                end
+                CONTROL_ADDR: begin
+                    control_reg <= wdata;
+                end
+            endcase
+        end
+    end
+end
+
+always_ff @( posedge clk or negedge rst_n ) begin 
+    if(!rst_n) begin
+        rdata <= 0;
+        rx_fifo_read <= 0;
+    end
+    else begin
+        rx_fifo_read <= 0;
+        if(read_en) begin
+            case (addr)
+                RX_ADDR: begin
+                    if(!rx_fifo_empty) begin
+                        rdata <= rx_fifo_rdata;
+                        rx_fifo_read <= 1;
+                    end
+                    else rdata <= 0;
+                end
+                STATUS_ADDR: begin
+                    rdata<= status_reg;
+                end
+                CONTROL_ADDR: begin
+                    rdata <= control_reg;
+                end 
+                default: rdata <= 0;
+            endcase
+        end
+    end
+    
+end
+
+always_comb begin
+    status_reg[0] = !rx_fifo_empty;
+    status_reg[1] = !tx_fifo_full;
+    status_reg[2] = parity_error;
+    status_reg[7:3] = 0;
+end
+
+assign parity_enable = control_reg[2];
+assign parity_odd = control_reg[3];
+
+assign irq = (status_reg[0] && control_reg[0]) || (status_reg[1] && control_reg[1]) || (status_reg[2]);
     
 endmodule
